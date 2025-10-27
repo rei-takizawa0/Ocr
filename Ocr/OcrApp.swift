ad0145774de6d3111d2fa4bb4d313b753e1c434d
Takizawa <rei971222@gmail.com>
2025-10-27 20:53:00 +0900

    Initial Commit

---
+++ Ocr/OcrApp.swift
@@ -0,0 +1,17 @@
+//
+//  OcrApp.swift
+//  Ocr
+//
+//  Created by takizawa rei on 2025/10/27.
+//
+
+import SwiftUI
+
+@main
+struct OcrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OcrTests/OcrTests.swift
@@ -0,0 +1,17 @@
+//
+//  OcrTests.swift
+//  OcrTests
+//
+//  Created by takizawa rei on 2025/10/27.
+//
+
+import Testing
+@testable import Ocr
+
+struct OcrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
