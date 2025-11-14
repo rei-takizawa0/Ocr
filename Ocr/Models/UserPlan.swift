92a023314a45cbac88b81950c5432acd58f4dd49
Takizawa <rei971222@gmail.com>
2025-11-14 18:54:49 +0900

    ユーザープランの追加

---
+++ Ocr/Models/UserPlan.swift
@@ -0,0 +1,38 @@
+//
+//  UserPlan.swift
+//  Ocr
+//
+//  Created by takizawa rei on 2025/11/13.
+//
+
+import Foundation
+
+struct UserPlan: Identifiable, Codable {
+    let id: UUID
+    let isShowAd: Bool
+    let ocrLimit: Int
+    let ocrUsedCount: Int
+
+    /// DTOからFollowモデルを初期化
+    init(dto: UserPlanDTO) {
+        self.id = dto.id
+        self.isShowAd = dto.is_show_ad
+        self.ocrLimit = dto.ocr_limit
+        self.ocrUsedCount = dto.ocr_used_count
+    }
+    
+    var remainingCount: Int {
+        max(ocrLimit - ocrUsedCount, 0)
+    }
+}
+
+struct UserPlanDTO: Codable {
+    let id: UUID
+    let is_show_ad: Bool
+    let ocr_limit: Int
+    let ocr_used_count: Int
+
+    var remainingCount: Int {
+        max(ocr_limit - ocr_used_count, 0)
+    }
+}
